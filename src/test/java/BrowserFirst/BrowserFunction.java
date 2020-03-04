afc9caad6fda505e0f280576e18333866a638b79
P10444427 <P10444427@192.168.2.2>
2020-03-04 15:21:12 +0000

    Comments updated

--- src/test/java/BrowserFirst/BrowserFunction.java
+++ src/test/java/BrowserFirst/BrowserFunction.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterSuite;
 
 public class BrowserFunction extends TestInMethod {
   TestInMethod ts = new TestInMethod();
+  //path of TestScript excel file
   static String fileName = "C:\\Users\\P10444427\\Desktop\\Java\\TestWorksheet.xlsx";
   static int i;
 	
@@ -22,10 +23,15 @@ public class BrowserFunction extends TestInMethod {
 
   @AfterMethod
   public void afterMethod() throws IOException {
+	  
+	  //Gets the current TestNG class Name
 	  String cellContent = this.getClass().getSimpleName();
 	  System.out.println(cellContent);
+	  //Searches for the Class name in Excel and return step name
 	  String StepName = ts.GetStepName(fileName, cellContent,i,7);
+	  //Searches for the Class name in Excel and returns description of step
 	  String StepDescrip = ts.GetStepName(fileName, cellContent,i,8);
+	  // Here we merge the step and description to be put in word
 	  String merged = StepName + ": "+ StepDescrip;
 	  System.out.println(merged);
 	  i++;
@@ -34,6 +40,7 @@ public class BrowserFunction extends TestInMethod {
 
   @BeforeClass
   public void beforeClass() throws IOException {
+	  //making iterator i=0 before every TestNG class
 	    i = 0;
   }
 
@@ -51,6 +58,7 @@ public class BrowserFunction extends TestInMethod {
 
   @AfterSuite
   public void afterSuite() {
+	  //finishing the excel instance
 	  ts.finish();
   }
 
--- src/test/java/BrowserFirst/TestInMethod.java
+++ src/test/java/BrowserFirst/TestInMethod.java
@@ -22,7 +22,9 @@ public class TestInMethod {
         /*
          * This method displays the total value of 
          */
+		//assigning rownr from findRow to row
         XSSFRow row = sheet.getRow(rownr);
+        //getting the cell content by using row from above and colnr parameter in GetStepName as j
         XSSFCell cell = row.getCell(colnr);
         String cellOut = ""+cell;    
                 return cellOut;
@@ -30,9 +32,13 @@ public class TestInMethod {
     }//end of output
 
 	private static int findRow(XSSFSheet sheet, String cellContent) {
+		//for loop to search each row in sheet
         for (Row row : sheet) {
+        	//for loop to search each cell in the above referenced row
             for (Cell cell : row) {
+            	//it will only look for celltype String
                 if (cell.getCellType() == CellType.STRING) {
+                	//This will take string from cell trim it and then compare to our given cellContent
                     if (cell.getRichStringCellValue().getString().trim().equals(cellContent)) {
                         return row.getRowNum();  
                     }
@@ -50,14 +56,20 @@ public class TestInMethod {
 
 	public String GetStepName(String fileName, String cellContent,int i,int j) throws IOException {
         int rownr=0, colnr = j;
+        //Opening the excel sheet at path fileName
         InputStream input = new FileInputStream(fileName);
-
+        // Using XSSFWorkbook to read the Excel Sheet
         XSSFWorkbook wb = new XSSFWorkbook(input);
+        //taking the first sheet(0) in the excel sheet 
         XSSFSheet sheet = wb.getSheetAt(0);
 
+        // this will find the row which has the Classname (in our Case Test Case Name)
         rownr = findRow(sheet, cellContent);
+        //adding the iterator i from aftermethod
         rownr = rownr+i;
+        //column index starts from 0 hence when you give col 8 it takes 7. hence subtracting 1
         colnr = colnr-1;
+        //this will search for the referenced cell with reference to cell that has Class name
         String giveBack = output(sheet, rownr, colnr);
         
         //finish();
