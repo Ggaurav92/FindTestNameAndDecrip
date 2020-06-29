20a6e415542c73187b7cc061a025764c26130a17
Ggaurav92 <g.gaurav1992@gmail.com>
2020-06-29 20:12:41 +0530

    this is the second commit by asian timezone guy

--- src/test/java/BrowserFirst/NewTest.java
+++ src/test/java/BrowserFirst/NewTest.java
@@ -15,12 +15,14 @@ public class NewTest extends BrowserFunction{
   @Test
   public void g1() {
 	  System.out.println("This is the change for git");
+	  System.out.println("This is the second change for git");
 	  Assert.assertEquals(true, true);
   }
   
   @Test
   public void g2() {
 	  System.out.println("This is the change for git");
+	  System.out.println("This is the second change for git");
 	  Assert.assertEquals(true, false);
   }
   
